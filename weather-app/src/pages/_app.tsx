d2e9f80e10bf4bc9a1d4a06896efa31478c4c362
Eryck Assis <eng.assis.dev@gmail.com>
2025-07-14 01:13:03 -0300

    Implement code changes to enhance functionality and improve performance

--- weather-app/src/pages/_app.tsx
+++ weather-app/src/pages/_app.tsx
@@ -4,5 +4,10 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+<>
+  <head>
+    <link rel="icon" href="/icons/bubu-Photoroom.png" />
+  </head>
+</>;
 
 export default MyApp;
