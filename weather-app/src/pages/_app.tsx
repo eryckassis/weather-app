f5c9ee2a26163f074fced305cd7af74807fe567b
Eryck Assis <eng.assis.dev@gmail.com>
2025-10-06 15:50:17 -0300

    Initialize MyApp component with global styles and favicon

--- weather-app/src/pages/_app.tsx
+++ weather-app/src/pages/_app.tsx
@@ -1,1 +1,16 @@
-import
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
+
+function MyApp({ Component, pagesProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/icons/bubu-Photoroom.png" />
+      </Head>
+      <Component {...pagesProps} />
+    </>
+  );
+}
+
+export default MyApp;
